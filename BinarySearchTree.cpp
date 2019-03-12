e3fbdb215c1347c8547d536e5bb84a07b77c54b2
Jacob Piirsalu <jacob.piirsalu@gmail.com>
2019-03-12 14:11:58 -0400

    changes to BinarySearchTree.cpp

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -1,6 +1,55 @@
 #include "BinarySearchTree.hpp"
+#include <cstdlib>
 
+using namespace std;
+typedef BinarySearchTree::DataType DataType
 
-// Implement the functions here.
+BinarySearchTree::BinarySearchTree()
+{
+
+}
+
+unsigned int BinarySearchTree::size() const
+{
+
+}
+
+BinarySearchTree::~BinarySearchTree()
+{
+
+}
+bool BinarySearchTree::insert(DataType val)
+{
+
+}
+
+bool BinarySearchTree::remove(DataType val)
+{
+
+}
+
+bool BinarySearchTree::exists(DataType val) const
+{
+
+}
+
+DataType BinarySearchTree::min() const
+{
+
+}
+
+DataType BinarySearchTree::max() const
+{
+
+}
+
+unsigned int  BinarySearchTree::depth() const
+{
+
+}
+void BinarySearchTree::print() const
+{
+
+}
 
 
