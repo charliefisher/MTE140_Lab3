71a24609a4b9b41d4697b0d13869db3342f0c3a8
Charlie Fisher <charliefisherhome@gmail.com>
2019-03-19 14:03:40 -0400

    Fixed efficiency and cleanliness of dequeue in PriorityQueue

--- PriorityQueue.cpp
+++ PriorityQueue.cpp
@@ -34,12 +34,8 @@ bool PriorityQueue::enqueue(DataType val) {
 	}
 	return true;
 }
-// Dequeues the top element with the maximum value (priority) and rearranges
-// the resulting heap. Returns false if failed, true otherwise.
+
 bool PriorityQueue::dequeue() {
-	//	cout << "called dequeue" << endl;
-	//	print();
-	//	cout << endl << "size " << size() << endl;
 	if (empty())
 		return false;
 	else if (size() == 1) {
@@ -49,49 +45,20 @@ bool PriorityQueue::dequeue() {
 	heap_[1] = heap_[size_]; // replace root with bottom right most leaf node
 	size_--; // decrement size as that part of the array should now be inaccessible
 
-	//keep switching root with largest child
-
-	int cur = 1, lastCur = 0;
-	do {
-		lastCur = cur;
-		if (size_ >= 2*cur + 1) { // has two child nodes
-			if (std::max(heap_[2*cur], heap_[2*cur+1]) > heap_[cur]) {
-				int maxChild = heap_[2 * cur] > heap_[2 * cur + 1] ? 2 * cur : 2 * cur + 1;
-				int temp = heap_[maxChild];
-				heap_[maxChild] = heap_[cur];
-				heap_[cur] = temp;
-				cur = maxChild;
-			}
-		}
-		else if(size_ >= 2*cur) { // only has a left child
-			if (heap_[2*cur] > heap_[cur]) {
-				int temp = heap_[cur];
-				heap_[cur] = heap_[cur*2];
-				heap_[cur*2] = temp;
-				cur = 2*cur;
-			}
-		}
-
-	} while (lastCur != cur);
-
-
-
-//cur = 1;
-//	while (std::max(heap_[2*cur], heap_[2*cur+1]) > heap_[cur] && cur >= size_) {
-//		int maxChild = heap_[2*cur] > heap_[2*cur+1]? 2*cur : 2*cur+1;
-//		int temp = heap_[maxChild];
-//		heap_[maxChild] = heap_[cur];
-//		heap_[cur] = temp;
-//		cur = maxChild;
-//	}
-//	print();
+	int cur = 1;
+	while (size_ >= 2*cur+1 && std::max(heap_[2*cur], heap_[2*cur+1]) > heap_[cur]) {
+		int maxChild = heap_[2 * cur] > heap_[2 * cur + 1] ? 2 * cur : 2 * cur + 1;
+		int temp = heap_[maxChild];
+		heap_[maxChild] = heap_[cur];
+		heap_[cur] = temp;
+		cur = maxChild;
+	}
+	if (size_ == 2*cur && heap_[2*cur] > heap_[cur]) {
+		int temp = heap_[2*cur];
+		heap_[2 * cur] = heap_[cur];
+		heap_[cur] = temp;
+	}
 	return true;
-//	else if (size_ <= 3) { // case where after removing root, we won't have multiple child nodes
-//			size_--;
-//			if ()
-//
-//			return true;
-//		}
 }
 
 DataType PriorityQueue::max() const {
